463b65e76b0c5e99e3ecd1d3522fcd31a35de377
tahayayci <62017947+tahayayci@users.noreply.github.com>
2020-03-19 17:00:59 +0300

    Depreciated signature-version problem is fixed.

--- cocktail_rating/settings.py
+++ cocktail_rating/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'cocktail-rating'
+AWS_S3_REGION_NAME = 'eu-central-1'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
