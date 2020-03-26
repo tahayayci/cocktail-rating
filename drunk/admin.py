c28dca18410a28cec99f547043e89988f4cf3c8d
tahayayci <62017947+tahayayci@users.noreply.github.com>
2020-03-26 08:55:56 +0300

    Update admin.py
    Admin page changes

--- drunk/admin.py
+++ drunk/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from drunk.models import Cocktail, CocktailGroup, CocktailReview, User
 
+
+class CocktailAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'group__name',
+        'is_alcoholic',
+        'is_active'
+    )
+
+
 admin.site.register(User)
-admin.site.register(Cocktail)
+admin.site.register(Cocktail, CocktailAdmin)
 admin.site.register(CocktailGroup)
 admin.site.register(CocktailReview)
